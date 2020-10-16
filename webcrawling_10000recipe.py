ca7d5df80379f82aec8cab22b8ebf953e9f68e4f
winni0520 <whekals@naver.com>
2020-10-16 09:14:02 +0900

    dir path change

--- webcrawling_10000recipe.py
+++ webcrawling_10000recipe.py
@@ -5,6 +5,7 @@ import urllib.request
 from urllib.request import Request, urlopen
 import json
 import os
+
 url ="https://www.10000recipe.com/recipe/6940325"
 res = requests.get(url)
 res.raise_for_status()
@@ -13,6 +14,7 @@ soup = BeautifulSoup(res.text, "lxml")
 """레시피 저장 폴더 생성"""
 dir_name = '\\recipe' #임시
 os.mkdir('C:\\Users\\damin-jo\\Desktop\\Python-WebScraping'+dir_name)
+file_path = "C:\\Users\\damin-jo\\Desktop\\Python-WebScraping"
 
 """img url 저장"""
 img_store = []
@@ -22,10 +24,10 @@ for i in range(1,20):
     img_store.append(str(recipe))
     img_store.append("\n")
 
-with open("C:\\Users\\damin-jo\\Desktop\\Python-WebScraping"+dir_name+"\\recipe_img.html", "w", encoding="utf-8") as f:
-    f.writelines(img_store)# 
-with open("C:\\Users\\damin-jo\\Desktop\\Python-WebScraping"+dir_name+"\\recipe_img.txt", "w", encoding="utf-8") as f:
-    f.writelines(img_store)#
+with open(file_path+dir_name+"\\recipe_img.html", "w", encoding="utf-8") as f:
+    f.writelines(img_store)
+with open(file_path+dir_name+"\\recipe_img.txt", "w", encoding="utf-8") as f:
+    f.writelines(img_store)
 
 def fileopen(data, target):
     with open(data, 'r', encoding='UTF-8') as file:
@@ -41,20 +43,20 @@ def count_word(data, target):
             count += 1
     return count
 
-filepath = "C:\\Users\\damin-jo\\Desktop\\Python-WebScraping"+dir_name+"\\recipe_img.txt"
+filepath = file_path+dir_name+"\\recipe_img.txt"
 target = "https://recipe1.ezmember.co.kr/"
 splitdata = fileopen(filepath, target)
 step_num = count_word(splitdata, target)
 
 """img url 추출"""
-recipe_file = open("C:\\Users\\damin-jo\\Desktop\\Python-WebScraping"+dir_name+"\\recipe_img.html", 'r', encoding='utf-8')
+recipe_file = open(file_path+dir_name+"\\recipe_img.html", 'r', encoding='utf-8')
 #저장공간은 위에서 만든 html파일 저장되는 곳으로 수정
 soup = BeautifulSoup(recipe_file, 'html.parser')
 
 for i in range(0, step_num):
     images = soup.findAll('img')[i]
     step_img = images.get("src")
-    urllib.request.urlretrieve(step_img, "C:\\Users\\damin-jo\\Desktop\\Python-WebScraping"+dir_name+"\\img"+str(i))
+    urllib.request.urlretrieve(step_img, file_path+dir_name+"\\img"+str(i))
     #print(step_img) #테스트용
 
 """레시피 내용 텍스트 파일로 저장"""
@@ -64,9 +66,9 @@ for i in range(1, step_num+1):
     recipe = soup.find_all("div",attrs={"id":recipe_step})
     for recipes in recipe:
         a = recipes.get_text()
-        #print(a)
-        step_store.append(a+"\n") #list안에 원소 없는 상태에서 store[i]=a로 지정 불가
+        #print(a) #test
+        step_store.append(a+"\n")
        
-with open("C:\\Users\\damin-jo\\Desktop\\Python-WebScraping"+dir_name+"\\recipe.txt", "w", encoding="utf-8") as f:
-    f.writelines(step_store)# 
+with open(file_path+dir_name+"\\recipe.txt", "w", encoding="utf-8") as f:
+    f.writelines(step_store)
 
